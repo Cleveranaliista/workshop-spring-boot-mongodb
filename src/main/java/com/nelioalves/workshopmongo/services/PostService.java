1697c884e762b6807c363194c93c9164c23ca934
Clever Junior <cleveranalista@gmail.com>
2022-04-06 00:30:21 -0300

    Consulta com vários critérios

--- src/main/java/com/nelioalves/workshopmongo/services/PostService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.nelioalves.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.workshopmongo.domain.Post;
-import com.nelioalves.workshopmongo.domain.User;
 import com.nelioalves.workshopmongo.repository.PostRepository;
 import com.nelioalves.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -28,6 +28,12 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text) {
 		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 *60 *60 *1000);
+		return repo.fullSearch(text, minDate, maxDate);
+		
 	}
 
 }
